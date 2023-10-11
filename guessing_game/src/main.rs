02f892efd1adc14050f4035b062411da96acda83
Bich Le <bcle00@gmail.com>
2023-10-10 18:25:46 -0700

    Rectangle: comparing println! and dbg!

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,20 @@
-struct User {
-    name: String,
-    age: i32,
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
 
-
-
 fn main() {
-    let u1 = User {name: String::from("John"), age: 15};
-    let u2 = User {age: 17, ..u1};
-    println!("User age: {}", u1.age);
-    println!("User name and age: {} {}", u2.name, u2.age);
+    let scale = 2;
+    let rect1 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    print_rect(&rect1);
+    dbg!(&rect1);
+}
+
+fn print_rect(r: &Rectangle) {
+    println!("w:{} h:{}", r.width, r.height)
 }

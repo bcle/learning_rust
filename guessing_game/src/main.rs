2bed4e46657c8f5c70a878e1715b1ab0bdba654a
Bich Le <bcle00@gmail.com>
2023-10-10 17:44:30 -0700

    struct copying: ok if reference only age field

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,6 +8,6 @@ struct User {
 fn main() {
     let u1 = User {name: String::from("John"), age: 15};
     let u2 = User {age: 17, ..u1};
-    println!("User name and age: {} {}", u1.name, u1.age);
+    println!("User age: {}", u1.age);
     println!("User name and age: {} {}", u2.name, u2.age);
 }

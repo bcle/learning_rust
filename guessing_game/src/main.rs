7362355f8de9afc0a2223b9ba69ffe9ec50dbb9e
Bich Le <bcle00@gmail.com>
2023-10-11 22:54:36 -0700

    closure: FnMut example with sort_by_key()

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,16 @@
-fn main() {
-    let mut list = vec![1, 2, 3];
-    println!("Before defining closure: {:?}", list);
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
-    let mut borrows_mutably = || list.push(7);
-    println!("Before calling closure: {:?}", list);
-    borrows_mutably();
-    println!("After calling closure: {:?}", list);
+fn main() {
+    let mut list = [
+        Rectangle { width: 10, height: 1 },
+        Rectangle { width: 3, height: 5 },
+        Rectangle { width: 7, height: 12 },
+    ];
+
+    list.sort_by_key(|r| r.width);
+    println!("{:#?}", list);
 }

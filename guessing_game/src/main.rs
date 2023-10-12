4845a92b0db690b0c2cfd8aa2b988ad4708d2947
Bich Le <bcle00@gmail.com>
2023-10-11 18:06:17 -0700

    trait example (tweet and summarize)

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,23 +1,16 @@
-fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
-    let mut largest = &list[0];
+pub mod aggregator;
 
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
+use aggregator::{Summary, Tweet};
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
 
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
+    println!("1 new tweet: {}", tweet.summarize());
 }

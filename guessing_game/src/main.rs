42b74adb9cf378ae5e3b665801c481e157352f41
Bich Le <bcle00@gmail.com>
2023-10-12 14:48:33 -0700

    smart pointer with Drop()

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,19 @@
-enum List {
-    Cons(i32, Box<List>),
-    Nil,
+struct CustomSmartPointer {
+    data: String,
 }
 
-use crate::List::{Cons, Nil};
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
 
 fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("CustomSmartPointers created.");
 }
